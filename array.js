c78982896ed40b37bd5a93f4e27984146e380ab2
egoleamaka <egoleamaka@gmail.com>
2022-10-03 16:09:27 -0700

    took off the first character

--- array.js
+++ array.js
@@ -7,5 +7,10 @@ let arr1 = []
 let fullName = 'amaka egole'
 
 //split your fullname
+let arr2 = fullName.split('')
 
-fullName.split('')
+//remove the first and last character
+arr2.shift()
+
+// let withoutFirstAndLast = male.slice(1, -1)
+console.log(arr2)
